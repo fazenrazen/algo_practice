67766f33e4f0d184806c27ed13eb597fd41b7545
fazenrazen <faizand@umich.edu>
2023-07-26 23:44:34 -0700

    Working on Ch 2

---
+++ selection_sort.py
@@ -0,0 +1,23 @@
+    
+"""
+Arrays 
+Pro: Finding an element and allocatiing space
+Con: Adding space or wastage of space
+
+Linked List
+Pro: easy to add space to the list
+Con: Have to go through one by one
+
+Array  
+Read/Find   O(1)  
+Insertion   O(n)
+
+Linked List
+Read/Find   O(n)
+Insertion   O(1)
+
+Chapter 2 Excerises (NOT CHECKED)
+
+
+
+"""
